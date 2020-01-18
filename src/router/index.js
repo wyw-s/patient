ef681351ea87ad00fd9ba5e4a3251a07ac5786dc
wyw-s <wyw.s@foxmail.com>
2020-01-18 11:04:05 +0800

    资源整合

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import login from '../views/login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'login',
+    component: login
   }
 ]
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>登录页</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style>
+</style>
